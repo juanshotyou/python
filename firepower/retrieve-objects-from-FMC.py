3a665071eb63b01983782b587df44ac238245821
Vlad R <vlad.raducanu@outlook.com>
2022-01-20 17:25:36 +0000

    Added simple object converter for Sonicwall UTM exports in JSON

---
+++ firepower/retrieve-objects-from-FMC.py
@@ -0,0 +1,59 @@
+import requests
+import json
+from datetime import datetime
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+SCRIPT_RUN_TIME = datetime.now().strftime("%d-%B-%Y-%I-%M-%p")
+
+#Get FMC IP and credentials and prepare auth cookie
+
+FMC_IP = input("\nPlease input the IP address of the FMC: ")
+USER = input("\nPlease input the username for the FMC: ")
+PASS = input("\nPlease input the password for the FMC: ")
+
+#Authenticate the session, retrieve domain UUID and print result
+
+AUTH_URL = "https://" + FMC_IP + "/api/fmc_platform/v1/auth/generatetoken"
+AUTH_RESPONSE = requests.post(AUTH_URL, auth = (USER, PASS), verify = False)
+
+AUTH_TOKEN = AUTH_RESPONSE.headers["X-auth-access-token"]
+AUTH_REFRESH_TOKEN = AUTH_RESPONSE.headers["X-auth-refresh-token"]
+DOMAIN_UUID = AUTH_RESPONSE.headers["DOMAIN_UUID"]
+HEADERS = {"X-auth-access-token" : AUTH_TOKEN}
+
+print("\nOperation returned code", AUTH_RESPONSE.status_code, "\n")
+
+#Prepare the URL and send the request for the objects
+
+NETWORK_OBJECTS_URL = "https://" + FMC_IP + "/api/fmc_config/v1/domain/" + DOMAIN_UUID + "/object/networks?expanded=true"
+HOST_OBJECTS_URL = "https://" + FMC_IP + "/api/fmc_config/v1/domain/" + DOMAIN_UUID + "/object/hosts?expanded=true"
+GROUP_OBJECTS_URL = "https://" + FMC_IP + "/api/fmc_config/v1/domain/" + DOMAIN_UUID + "/object/networkgroups?expanded=true"
+URL_OBJECTS_URL = "https://" + FMC_IP + "/api/fmc_config/v1/domain/" + DOMAIN_UUID + "/object/urls?expanded=true"
+FQDN_OBJECTS_URL = "https://" + FMC_IP + "/api/fmc_config/v1/domain/" + DOMAIN_UUID + "/object/fqdns?expanded=true"
+
+REQUIRED_DATA = (NETWORK_OBJECTS_URL, HOST_OBJECTS_URL, GROUP_OBJECTS_URL, URL_OBJECTS_URL)
+RECEIVED_DATA = []
+
+for item in REQUIRED_DATA:
+    response = requests.get(item, headers = HEADERS, verify = False)
+    json_response = json.loads(response.text)
+    RECEIVED_DATA.append(json_response["items"])
+
+
+for item in RECEIVED_DATA:
+    if item[0]["type"] == "Network":
+        with open("network-objects-" + SCRIPT_RUN_TIME + ".txt", "a+") as file:
+            json.dump(item, file, indent = 2)
+    elif item[0]["type"] == "Host":
+        with open("host-objects-" + SCRIPT_RUN_TIME + ".txt", "a+") as file:
+            json.dump(item, file, indent = 2)
+    elif item[0]["type"] == "NetworkGroup":
+        with open("group-objects-" + SCRIPT_RUN_TIME + ".txt", "a+") as file:
+            json.dump(item, file, indent = 2)
+    elif item[0]["type"] == "Url":
+        with open("url-objects-" + SCRIPT_RUN_TIME + ".txt", "a+") as file:
+            json.dump(item, file, indent = 2)
+    elif item[0]["type"] == "FQDN":
+        with open("fqdn-objects-" + SCRIPT_RUN_TIME + ".txt", "a+") as file:
+            json.dump(item, file, indent = 2)
---
+++ firepower/sonicwall-to-ftd.py
@@ -0,0 +1,101 @@
+import json
+import os
+from netaddr import IPAddress
+from datetime import datetime
+
+SCRIPT_RUN_TIME = datetime.now().strftime("%d-%B-%Y-%I-%M-%p")
+
+#Check current working directory and make sure it's set as the working folder
+
+path = os.getcwd()
+print("\nInitial path was:", path)
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+path = os.getcwd()
+print("\nNew path is:", path)
+
+#Get the objects filename and read the data
+
+filename = input("\nPlease input the name of the file storing the network objects.\nMake sure to include the file extension: ")
+
+with open(filename) as f:
+    objects_json = json.load(f)
+
+#Loop through the data and create Firepower equivalent
+
+index = 0
+successes = 0
+failures = 0
+
+ipv4_host_object = {}
+ipv4_network_object = {}
+ipv4_range_object = {}
+ipv4_host_object_list = []
+ipv4_network_object_list = []
+ipv4_range_object_list = []
+
+fqdn_object = {}
+fqdn_objects_list = []
+
+for object in objects_json["address_objects"]:
+    if "ipv4" in object:
+        if "host" in object["ipv4"] and object["ipv4"]["host"] != {}:
+            ipv4_host_object["type"] = "Host"
+            ipv4_host_object["value"] = object["ipv4"]["host"]["ip"]
+            ipv4_host_object["name"] = object["ipv4"]["name"]
+            ipv4_host_object["description"] = "Added via the FMC API with index " + str(index)
+            ipv4_host_object_list.append(ipv4_host_object)
+            ipv4_host_object = {}
+            successes+=1
+        elif "network" in object["ipv4"] and object["ipv4"]["network"] != {}:
+            ipv4_network_object["type"] = "Network"
+            ipv4_network_object["value"] = object["ipv4"]["network"]["subnet"] + "/" + str(IPAddress(object["ipv4"]["network"]["mask"]))
+            ipv4_network_object["name"] = object["ipv4"]["name"]
+            ipv4_network_object["description"] = "Added via the FMC API with index " + str(index)
+            ipv4_network_object_list.append(ipv4_network_object)
+            ipv4_network_object = {}
+            successes+=1
+        elif "range" in object["ipv4"]:
+            ipv4_range_object["type"] = "Range"
+            ipv4_range_object["value"] = object["ipv4"]["range"]["begin"] + "-" + object["ipv4"]["range"]["end"]
+            ipv4_range_object["name"] = object["ipv4"]["name"]
+            ipv4_range_object["description"] = "Added via the FMC API with index " + str(index)
+            ipv4_range_object_list.append(ipv4_range_object)
+            ipv4_range_object = {}
+            successes+=1
+        else:
+            print(f"\nItem at index {index} does not meet criteria so it has been ignored.")
+            print("Offending item: \n", object["ipv4"])
+            failures+=1
+    if "mac" in object:
+        print(f"\nItem at index {index} does not meet criteria so it has been ignored.")
+        print("Offending item: \n", object["mac"])
+        failures+=1
+    if "fqdn" in object:
+        fqdn_object["type"] = "FQDN"
+        fqdn_object["name"] = object["fqdn"]["name"]
+        fqdn_object["value"] = object["fqdn"]["domain"]
+        fqdn_object["description"] = "Added via the FMC API with index " + str(index)
+        fqdn_object["dnsResolution"] = "IPV4_ONLY"
+        fqdn_objects_list.append(fqdn_object)
+        fqdn_object = {}
+        successes+=1
+    if "ipv6" in object:
+        print(f"\nItem at index {index} does not meet criteria so it has been ignored.")
+        print("Offending item: \n", object["ipv6"])
+        failures+=1
+    index+=1
+        
+print(f"\nScript has processed {index} items with {successes} successfull and {failures} failed translations ")
+
+#Save each object sets to its own file
+
+with open("sonicwall-to-ftd-host-objects-" + SCRIPT_RUN_TIME + ".txt", "a+") as f:
+    json.dump(ipv4_host_object_list, f, indent = 2)
+with open("sonicwall-to-ftd-network-objects-" + SCRIPT_RUN_TIME + ".txt", "a+") as f:
+    json.dump(ipv4_network_object_list, f, indent = 2)
+with open("sonicwall-to-ftd-range-objects-" + SCRIPT_RUN_TIME + ".txt", "a+") as f:
+    json.dump(ipv4_range_object_list, f, indent = 2)
+with open("sonicwall-to-ftd-fqdn-objects-" + SCRIPT_RUN_TIME + ".txt", "a+") as f:
+    json.dump(fqdn_objects_list, f, indent = 2)
