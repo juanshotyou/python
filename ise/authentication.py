b16984b1e02093cb760a98865a97b2c6a30a9d1d
Vlad R <vlad.raducanu@dnaclab.net>
2022-06-17 17:15:34 +0000

    Switched credential input from interactive to envrionment.

--- ise/authentication.py
+++ ise/authentication.py
@@ -1,22 +1,34 @@
 import requests
 import urllib3
 import base64
+import json
+import os
+from dotenv import load_dotenv
 from datetime import datetime
 from getpass import getpass
 
+load_dotenv()
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 ISE_IP = "10.221.0.101:9060"
 ISE_FQDN = "ise.dnaclab.net:9060"
+ISE_USER = os.environ.get("USERNAME")
+ISE_PASS = os.environ.get("PASSWORD")
 
 
 def testAuthentication():
-    username = input("Please input the ISE username: ")
-    password = getpass("Please input the ISE password: ")
+    # Enable if interactive authentication is required
+    # username = input("Please input the ISE username: ")
+    # password = getpass("Please input the ISE password: ")
+
+    # Enable if credentials are present in the environment
+
+    username = ISE_USER
+    password = ISE_PASS
 
     auth2encode = username + ":" + password
     auth = base64.b64encode(auth2encode.encode('UTF-8')).decode('ASCII')
-    url = ISE_IP + "/ers/config/sessionservicenode/versioninfo"
+    url = "https://" + ISE_IP + "/ers/config/deploymentinfo/getAllInfo"
     headers = {
         "Accept": "application/json",
         "Authorization": "Basic " + auth,
@@ -24,10 +36,12 @@ def testAuthentication():
     }
 
     response = requests.get(url, headers=headers, verify = False)
-    prrint(response)
+    data = json.loads(response.content)
+    print(json.dumps(data, indent = 4))
 
 def main():
     testAuthentication()
+    
 
 if __name__ == "__main__":
     global script_run_time 
