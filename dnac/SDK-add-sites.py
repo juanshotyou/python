40433c79018cf2b16be921b244f11c3c8e877fb8
Vlad Raducanu <vlad.raducanu@outlook.com>
2022-07-06 10:34:45 +0100

    Housekeeping

--- dnac/SDK-add-sites.py
+++ dnac/SDK-add-sites.py
@@ -22,9 +22,9 @@ def inventoryAudit(dnac):
     except ApiError as e:
         print("Error encountered! See below for more info:\n",e)
 
-def addSiteToHierarchy(dnac, list_of_sites):
+def addSiteToHierarchy(dnac, sites_to_add):
 
-    for site in list_of_sites:
+    for site in sites_to_add:
         # Start by assuming that fields are missing until we check against the data frame
         area_check = False
         building_check = False
@@ -41,6 +41,8 @@ def addSiteToHierarchy(dnac, list_of_sites):
             floor_payload = json.loads(J2_TMP_FLOOR.render(site))
             floor_check = True
         
+        print(area_check, building_check, floor_check)
+
         # Load area to DNAC
         if area_check:
             try:
@@ -66,6 +68,8 @@ def addSiteToHierarchy(dnac, list_of_sites):
                     sys.exit()
             except ApiError as e:
                 print("Error encountered! See below for more info:\n",e)
+            except TypeError as e:
+                print("Type error encountered! See below for more info:\n",e)
 
         # Load building to DNAC
         if building_check:
@@ -92,6 +96,8 @@ def addSiteToHierarchy(dnac, list_of_sites):
                     sys.exit()
             except ApiError as e:
                 print("Error encountered! See below for more info:\n",e)
+            except TypeError as e:
+                print("Type error encountered! See below for more info:\n",e)
 
         # Load floor to DNAC
         if floor_check:
@@ -116,8 +122,52 @@ def addSiteToHierarchy(dnac, list_of_sites):
                 else:
                     print("Task ID could not be retrieved! Terminating program!")
                     sys.exit()
-            except ApiError as e:
-                print("Error encountered! See below for more info:\n",e)
+            except TypeError as e:
+                print("Type error encountered! See below for more info:\n",e)
+
+def deleteSiteFromHierarchy(dnac, sites_to_add, site_hierarchy):
+
+    for i in site_hierarchy["response"]:
+        for k in i["additionalInfo"]:
+            if k["nameSpace"] == "Location" and k["attributes"]["type"] == "floor":
+                for j in sites_to_add:
+                    if j["Floor"] == i["name"]:
+                        try:
+                            result = dnac.sites.delete_site(site_id=i["id"])
+                            print(f"Floor {i['name']} successfully deleted!")
+                        except ApiError as e:
+                            print("Error encountered! See below for more info:\n",e)
+  
+    for i1 in site_hierarchy["response"]:
+        for k1 in i1["additionalInfo"]:
+            if k1["nameSpace"] == "Location" and k1["attributes"]["type"] == "building":
+                for j1 in sites_to_add:
+                    if j1["Building"] == i1["name"]:
+                        try:
+                            result = dnac.sites.delete_site(site_id=i1["id"])
+                            print(f"Building {i1['name']} successfully deleted!")
+                        except ApiError as e:
+                            print("Error encountered! See below for more info:\n",e)
+
+    for i2 in site_hierarchy["response"]:
+        for k2 in i2["additionalInfo"]:
+            if k2["nameSpace"] == "Location" and k2["attributes"]["type"] == "area":
+                for j2 in sites_to_add:
+                    if j2["Site"] == i2["name"]:
+                        try:
+                            result = dnac.sites.delete_site(site_id=i2["id"])
+                            print(f"Area {i2['name']} successfully deleted!")
+                        except ApiError as e:
+                            print("Error encountered! See below for more info:\n",e)
+
+
+def getSiteHierarchy(dnac):
+
+    try:
+        site_hierarchy = dnac.sites.get_site()
+        return site_hierarchy
+    except ApiError as e:
+        print("Error encountered! See below for more info:\n",e)
 
 def checkTaskExecutionStandard(dnac, taskId):
 
@@ -145,9 +195,12 @@ def main():
     dnac = DNACenterAPI()
     # inventoryAudit(dnac)
     # Retrieve sites, buildings and floors from data capture file
-    list_of_sites = readDataCaptureFile()
+    sites_to_add = readDataCaptureFile()
+    site_hierarchy = getSiteHierarchy(dnac)
+    # print(json.dumps(site_hierarchy["response"], indent = 4))
     # Add data to DNAC
-    addSiteToHierarchy(dnac, list_of_sites)
+    # addSiteToHierarchy(dnac, sites_to_add)
+    deleteSiteFromHierarchy(dnac, sites_to_add, site_hierarchy)
 
 if __name__ == "__main__":
     main()
